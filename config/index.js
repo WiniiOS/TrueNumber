5702e7745f84ae4729ab4ec0a5c7d25670b26090
Franck <franckndi5@gmail.com>
2025-07-21 03:01:41 +0100

    update config for production

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongoose: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/auth-service',
+    url: process.env.MONGODB_URI,
+    // url: process.env.MONGODB_URI || 'mongodb://localhost:27017/auth-service',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true
